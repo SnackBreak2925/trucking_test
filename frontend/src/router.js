7570478aed323b3799083b97b94b87fcb34e4e97
Nurd Galim <nurd_galim@mail.ru>
2022-08-30 16:54:47 +0700

    view fixes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,16 +1,18 @@
-import * as createRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Post from '@/components/PostIndex'
 import Author from '@/components/AuthorIndex'
 import PostsByTag from '@/components/PostsByTag'
 import AllPosts from '@/components/AllPosts'
 
-const router = createRouter(
-    [
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
         { path: '/author/:username', component: Author },
         { path: '/post/:slug', component: Post },
         { path: '/tag/:tag', component: PostsByTag },
         { path: '/', component: AllPosts },
     ]
+}
 )
 
 export default router
